5bbe80f7e439283e905e2dcd22591ae178358ac1
SzymonSzczotKivi <szymon.szczot@kivy.pl>
2024-05-04 17:22:26 +0200

    media

--- server/config/media.py
+++ server/config/media.py
@@ -5,4 +5,5 @@ from server.config.settings import get_settings
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 MEDIA_DIR = os.path.join(BASE_DIR, 'media')
+os.path.exists(MEDIA_DIR) or os.makedirs(MEDIA_DIR)
 MEDIA_URL = get_settings().api_domain + '/media/'
